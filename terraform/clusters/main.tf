eff305a6a9fb33a21034d9662add1825d2bba3bf
Ed Adkins <ejmadkins@gmail.com>
2022-03-09 14:36:38 +0000

    add WI support for image serve app

--- terraform/clusters/main.tf
+++ terraform/clusters/main.tf
@@ -14,3 +14,11 @@ module "gke_east" {
   }
   datapath_provider = var.enable_dataplane_v2 ? "ADVANCED_DATAPATH" : "DATAPATH_PROVIDER_UNSPECIFIED"
 }
+
+module "image-serve-workload-identity" {
+  source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  name       = "image-serve"
+  namespace  = "tenant-a"
+  project_id = var.project
+  roles      = ["roles/storage.admin"]
+}
